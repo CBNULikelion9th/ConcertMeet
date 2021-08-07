6822687a8de70c21cc8a4f70561f601234cc9157
y2336789 <you@example.com>
2021-08-07 23:23:22 +0900

    카테고리 추가

--- meetapp/static/js/content.js
+++ meetapp/static/js/content.js
@@ -1,7 +1,8 @@
-const hip_btn = document.querySelector("#hip");
-const bal_btn = document.querySelector(".bal");
+(() => {
+  const hip_btn = document.querySelector("#hip");
+  const bal_btn = document.querySelector("#bal");
+  const fes_btn = document.querySelector("#fes");
 
-function separate_genre() {
   const target = document.querySelectorAll(".concert_genre");
   target.forEach((item) => {
     var gen = item.innerText;
@@ -9,30 +10,45 @@ function separate_genre() {
       item.parentNode.classList.add("hiphop");
     } else if (gen === "인디") {
       item.parentNode.classList.add("indi");
+    } else if (gen === "페스티벌") {
+      item.parentNode.classList.add("festival");
     }
   });
-}
 
-hip_btn.addEventListener("click", () => {
-  const clear = document.querySelectorAll("div.line");
-  clear.forEach((item) => {
-    item.classList.remove("hide");
+  hip_btn.addEventListener("click", () => {
+    const clear = document.querySelectorAll("div.line");
+    clear.forEach((item) => {
+      item.classList.remove("hide");
+    });
+    const target = document.querySelectorAll(
+      "div.line.indi, div.line.festival"
+    );
+    target.forEach((item) => {
+      item.classList.toggle("hide");
+    });
   });
-  const target = document.querySelectorAll("div.line.indi");
-  target.forEach((item) => {
-    item.classList.toggle("hide");
-  });
-});
 
-bal_btn.addEventListener("click", () => {
-  const clear = document.querySelectorAll("div.line");
-  clear.forEach((item) => {
-    item.classList.remove("hide");
+  bal_btn.addEventListener("click", () => {
+    const clear = document.querySelectorAll("div.line");
+    clear.forEach((item) => {
+      item.classList.remove("hide");
+    });
+    const target = document.querySelectorAll(
+      "div.line.hiphop, div.line.festival"
+    );
+    target.forEach((item) => {
+      item.classList.toggle("hide");
+    });
   });
-  const target = document.querySelectorAll("div.line.hiphop");
-  target.forEach((item) => {
-    item.classList.toggle("hide");
-  });
-});
 
-separate_genre();
+  fes_btn.addEventListener("click", () => {
+    const clear = document.querySelectorAll("div.line");
+    clear.forEach((item) => {
+      item.classList.remove("hide");
+    });
+    const target = document.querySelectorAll("div.line.hiphop, div.line.indi");
+    target.forEach((item) => {
+      item.classList.toggle("hide");
+    });
+  });
+})();
