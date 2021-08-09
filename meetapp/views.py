b2cc7ec0d027dafd23c32a6554297960632149b2
Ju-wang <charles09099@gmail.com>
2021-08-09 22:41:58 +0900

    paginator

--- meetapp/views.py
+++ meetapp/views.py
@@ -1,5 +1,7 @@
 import json
 from django.contrib.auth.decorators import login_required
+from django.core import paginator
+from django.core.paginator import EmptyPage, Paginator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
@@ -7,21 +9,29 @@ from django.shortcuts import get_object_or_404, render, redirect
 from .forms import *
 from .models import Post, Comment, Declaration
 
+
 def post_home(request):
     post_home = Post.objects.all()
     concert_list = Concert.objects.all()
     context = {
         'post_home': post_home,
-        'concert_list' : concert_list,
+        'concert_list': concert_list,
     }
     return render(request, 'meetapp/post_home.html', context)
 
+
 def post_list(request):
+    page = request.GET.get("page", 1)
     post_list = Post.objects.all()
-    context = {
-        'post_list': post_list,
-    }
-    return render(request, 'meetapp/post_list.html', context)
+    paginator = Paginator(post_list, 10, orphans=3)
+    try:
+        lists = paginator.page(int(page))
+        return render(request, "meetapp/post_list.html", {
+            "page": lists
+        })
+    except EmptyPage:
+        return redirect("/")
+
 
 @login_required
 @require_POST
@@ -35,44 +45,48 @@ def post_like(request):
     else:
         post.likes_user.add(user)
 
-    context = {'likes_count':post.count_likes_user()}
+    context = {'likes_count': post.count_likes_user()}
     return HttpResponse(json.dumps(context), content_type="application/json")
 
 
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
-    post.hit +=1
+    post.hit += 1
     post.save()
-    form = CommentForm() 
+    form = CommentForm()
     context = {
-            'post': post,
-            'form': form,
-        }
+        'post': post,
+        'form': form,
+    }
     return render(request, 'meetapp/post_detail.html', context)
 
-def post_resethit(request,post_id):
+
+def post_resethit(request, post_id):
     post = Post.objects.get(id=post_id)
     post.views = 0
     post.save()
     return render(request, 'meetapp/post_detail.html')
 
+
 def post_new(request):
     if request.method == 'GET':
-        #빈 폼 보여주는 부분
+        # 빈 폼 보여주는 부분
         form = PostForm()
 
     elif request.method == 'POST':
         # 사용자가 입력한 데이터를 저장하는 부분
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            post = form.save(commit=False) #post.id 없음
-            post.user = request.user 
-            post.save() #post.id 저장
+            post = form.save(commit=False)  # post.id 없음
+            post.user = request.user
+            post.save()  # post.id 저장
             return redirect('meetapp:post_detail', post_id=post.id)
 
     return render(request, 'meetapp/post_new.html', {
         'form': form,
     })
+
+
 def post_edit(request, post_id):
     post = Post.objects.get(id=post_id)
 
@@ -83,7 +97,7 @@ def post_edit(request, post_id):
         # 사용자가 입력한 데이터를 저장하는 부분
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post.hit -= 1 
+            post.hit -= 1
             post = form.save()
             return redirect('meetapp:post_detail', post_id=post.id)
 
@@ -92,15 +106,17 @@ def post_edit(request, post_id):
         'post': post,
     })
 
+
 def post_delete(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     post.delete()
     return redirect('meetapp:post_list')
 
+
 @login_required
 def post_declaration(request, post_id):
     post = Post.objects.get(id=post_id)
-    
+
     if request.method == 'GET':
         form = DeclareForm()
 
@@ -108,11 +124,11 @@ def post_declaration(request, post_id):
         form = DeclareForm(request.POST)
         if form.is_valid():
             declaration = form.save(commit=False)
-            declaration.user = post.user 
+            declaration.user = post.user
             declaration.post = post
-            declaration.save() 
+            declaration.save()
             return redirect('meetapp:post_detail', post_id=post.id)
-            
+
     return render(request, 'meetapp/post_declaration.html', {
         'form': form,
     })
@@ -128,12 +144,13 @@ def comment_new(request, post_id):
             comment.post = post
             comment.user = request.user
             comment.save()
-            return redirect('meetapp:post_detail',post.id)
-    return redirect('meetapp:post_detail',post.id)
+            return redirect('meetapp:post_detail', post.id)
+    return redirect('meetapp:post_detail', post.id)
+
 
 @login_required
-def comment_edit(request, post_id,id):
-    comment = get_object_or_404(Comment,id=id)
+def comment_edit(request, post_id, id):
+    comment = get_object_or_404(Comment, id=id)
     if comment.user != request.user:
         return redirect('meetapp:post_detail', post_id)
     if request.method == 'POST':
@@ -141,24 +158,26 @@ def comment_edit(request, post_id,id):
         if form.is_valid():
             comment = form.save()
             comment.save()
-            return redirect('meetapp:post_detail',post_id)
+            return redirect('meetapp:post_detail', post_id)
     else:
-        form = CommentForm(instance=comment) 
+        form = CommentForm(instance=comment)
     return render(request, 'meetapp/comment_form.html', {
-        'form' : form,
+        'form': form,
     })
 
+
 @login_required
-def comment_delete(request, post_id,id):
-    comment = get_object_or_404(Comment,id=id)
+def comment_delete(request, post_id, id):
+    comment = get_object_or_404(Comment, id=id)
     if comment.user != request.user:
         return redirect('meetapp:post_detail', post_id)
     comment.delete()
     return redirect('meetapp:post_detail', post_id)
 
+
 def content_list(request):
     concert_list = Concert.objects.all()
     context = {
-        'concert_list' : concert_list,
+        'concert_list': concert_list,
     }
     return render(request, 'meetapp/content_list.html', context)
