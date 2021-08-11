7167e513b59d2a487c89ed896614491be1d1c394
y2336789 <you@example.com>
2021-08-11 20:25:15 +0900

    음악 장르별 크롤링 노출 마무리 및 링크 연결

--- parser.py
+++ parser.py
@@ -8,7 +8,7 @@ import django
 django.setup()
 from meetapp.models import Concert
 
-def start():
+def hiphop():
     html = requests.get("http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv&SubCa=Rap#")
     soup = BeautifulSoup(html.text, 'html.parser')
     html.close()
@@ -27,6 +27,8 @@ def start():
         img = li.find('img')
         img_src = img['src']
         title = img['alt']
+        link = li.find("a")["href"]
+        f_link = "http://ticket.interpark.com" + link;
 
         for el in li.find_all('td', {'class' : 'Rkdate'}):
             test = el.get_text()
@@ -38,27 +40,29 @@ def start():
             else:
                 location = test2
 
+        # print(f_link);
         concert_obj = {
             'title' : title,
             'image_url' : img_src,
             'location' : location,
-            'date' : date
+            'date' : date,
+            'link' : f_link
         }
         data.append(concert_obj)
 
-    print(data)
+    # print(data)
 
     for item in data:
-        print(item['image_url'])
+        # print(item['image_url'])
         try:
-            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="힙합").save()    
+            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="힙합", link=item['link']).save()    
             # print("new item added!! " + item['title'])
         except:
             # 데이터 중복 시 저장 불가
             print("중복")
     return data
 
-def start2():
+def indi():
     html = requests.get("http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv&SubCa=Ind")
     soup = BeautifulSoup(html.text, 'html.parser')
     html.close()
@@ -77,6 +81,8 @@ def start2():
         img = li.find('img')
         img_src = img['src']
         title = img['alt']
+        link = li.find("a")["href"]
+        f_link = "http://ticket.interpark.com" + link;
 
         for el in li.find_all('td', {'class' : 'Rkdate'}):
             test = el.get_text()
@@ -92,23 +98,24 @@ def start2():
             'title' : title,
             'image_url' : img_src,
             'location' : location,
-            'date' : date
+            'date' : date,
+            'link' : f_link
         }
         data.append(concert_obj)
 
-    print(data)
+    # print(data)
 
     for item in data:
-        print(item['image_url'])
+        # print(item['image_url'])
         try:
-            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="인디").save()    
+            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="인디", link=item['link']).save()        
             # print("new item added!! " + item['title'])
         except:
             # 데이터 중복 시 저장 불가
             print("중복")
     return data
 
-def start3():
+def festival():
     html = requests.get("http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv&SubCa=Fes#")
     soup = BeautifulSoup(html.text, 'html.parser')
     html.close()
@@ -127,6 +134,8 @@ def start3():
         img = li.find('img')
         img_src = img['src']
         title = img['alt']
+        link = li.find("a")["href"]
+        f_link = "http://ticket.interpark.com" + link;
 
         for el in li.find_all('td', {'class' : 'Rkdate'}):
             test = el.get_text()
@@ -142,23 +151,241 @@ def start3():
             'title' : title,
             'image_url' : img_src,
             'location' : location,
-            'date' : date
+            'date' : date,
+            'link' : f_link
         }
         data.append(concert_obj)
 
-    print(data)
+    # print(data)
 
     for item in data:
-        print(item['image_url'])
+        # print(item['image_url'])
         try:
-            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="페스티벌").save()    
+            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="페스티벌", link=item['link']).save()        
             # print("new item added!! " + item['title'])
         except:
             # 데이터 중복 시 저장 불가
             print("중복")
     return data
 
+def ballad():
+    html = requests.get("http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv&SubCa=Bal#")
+    soup = BeautifulSoup(html.text, 'html.parser')
+    html.close()
+
+    data1_list=soup.find_all('tbody')
+
+    data = []
+    li_list = []
+    location=''
+    date=''
+
+    for data1 in data1_list:
+        li_list.extend(data1.findAll('tr')) 
+
+    for li in li_list:
+        img = li.find('img')
+        img_src = img['src']
+        title = img['alt']
+        link = li.find("a")["href"]
+        f_link = "http://ticket.interpark.com" + link;
+
+        for el in li.find_all('td', {'class' : 'Rkdate'}):
+            test = el.get_text()
+            test2 = re.sub('\r\n\t\t\t\t\t\t\t', '', test)
+            check = len(test2)
+            # 날짜와 장소 구분
+            if check == 23 :
+                date = test2
+            else:
+                location = test2
+
+        concert_obj = {
+            'title' : title,
+            'image_url' : img_src,
+            'location' : location,
+            'date' : date,
+            'link' : f_link
+        }
+        data.append(concert_obj)
+
+    # print(data)
+
+    for item in data:
+        # print(item['image_url'])
+        try:
+            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="발라드", link=item['link']).save()        
+            # print("new item added!! " + item['title'])
+        except:
+            # 데이터 중복 시 저장 불가
+            print("중복")
+    return data
+
+def rock():
+    html = requests.get("http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv&SubCa=Roc#")
+    soup = BeautifulSoup(html.text, 'html.parser')
+    html.close()
+
+    data1_list=soup.find_all('tbody')
+
+    data = []
+    li_list = []
+    location=''
+    date=''
+
+    for data1 in data1_list:
+        li_list.extend(data1.findAll('tr')) 
+
+    for li in li_list:
+        img = li.find('img')
+        img_src = img['src']
+        title = img['alt']
+        link = li.find("a")["href"]
+        f_link = "http://ticket.interpark.com" + link;
+
+        for el in li.find_all('td', {'class' : 'Rkdate'}):
+            test = el.get_text()
+            test2 = re.sub('\r\n\t\t\t\t\t\t\t', '', test)
+            check = len(test2)
+            # 날짜와 장소 구분
+            if check == 23 :
+                date = test2
+            else:
+                location = test2
+
+        concert_obj = {
+            'title' : title,
+            'image_url' : img_src,
+            'location' : location,
+            'date' : date,
+            'link' : f_link
+        }
+        data.append(concert_obj)
+
+    # print(data)
+
+    for item in data:
+        # print(item['image_url'])
+        try:
+            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="락", link=item['link']).save()        
+            # print("new item added!! " + item['title'])
+        except:
+            # 데이터 중복 시 저장 불가
+            print("중복")
+    return data
+
+def jazz():
+    html = requests.get("http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv&SubCa=Jaz#")
+    soup = BeautifulSoup(html.text, 'html.parser')
+    html.close()
+
+    data1_list=soup.find_all('tbody')
+
+    data = []
+    li_list = []
+    location=''
+    date=''
+
+    for data1 in data1_list:
+        li_list.extend(data1.findAll('tr')) 
+
+    for li in li_list:
+        img = li.find('img')
+        img_src = img['src']
+        title = img['alt']
+        link = li.find("a")["href"]
+        f_link = "http://ticket.interpark.com" + link;
+
+        for el in li.find_all('td', {'class' : 'Rkdate'}):
+            test = el.get_text()
+            test2 = re.sub('\r\n\t\t\t\t\t\t\t', '', test)
+            check = len(test2)
+            # 날짜와 장소 구분
+            if check == 23 :
+                date = test2
+            else:
+                location = test2
+
+        concert_obj = {
+            'title' : title,
+            'image_url' : img_src,
+            'location' : location,
+            'date' : date,
+            'link' : f_link
+        }
+        data.append(concert_obj)
+
+    # print(data)
+
+    for item in data:
+        # print(item['image_url'])
+        try:
+            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="재즈", link=item['link']).save()        
+            # print("new item added!! " + item['title'])
+        except:
+            # 데이터 중복 시 저장 불가
+            print("중복")
+    return data
+
+def pop():
+    html = requests.get("http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv&SubCa=For#")
+    soup = BeautifulSoup(html.text, 'html.parser')
+    html.close()
+
+    data1_list=soup.find_all('tbody')
+
+    data = []
+    li_list = []
+    location=''
+    date=''
+
+    for data1 in data1_list:
+        li_list.extend(data1.findAll('tr')) 
+
+    for li in li_list:
+        img = li.find('img')
+        img_src = img['src']
+        title = img['alt']
+        link = li.find("a")["href"]
+        f_link = "http://ticket.interpark.com" + link;
+
+        for el in li.find_all('td', {'class' : 'Rkdate'}):
+            test = el.get_text()
+            test2 = re.sub('\r\n\t\t\t\t\t\t\t', '', test)
+            check = len(test2)
+            # 날짜와 장소 구분
+            if check == 23 :
+                date = test2
+            else:
+                location = test2
+
+        concert_obj = {
+            'title' : title,
+            'image_url' : img_src,
+            'location' : location,
+            'date' : date,
+            'link' : f_link
+        }
+        data.append(concert_obj)
+
+    # print(data)
+
+    for item in data:
+        # print(item['image_url'])
+        try:
+            Concert(title = item['title'], image_url=item['image_url'], location=item['location'], date=item['date'], genre="내한공연", link=item['link']).save()        
+            # print("new item added!! " + item['title'])
+        except:
+            # 데이터 중복 시 저장 불가
+            print("중복")
+    return data
+
+
 if __name__ == '__main__':
-    start()
-    start2()
-    start3()
+    hiphop()
+    ballad()
+    pop()
+    rock()
+    jazz()
+    festival()
+    indi()
